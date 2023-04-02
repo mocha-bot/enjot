4487f1e60807e42b030056c4490c0c32ef992516
Risky Feryansyah <riskypribadi24@gmail.com>
2023-04-02 20:38:53 +0700

    chore: init enjot project structure for dummy (#1)

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/mocha-bot/enjot
+
+go 1.19
+
+require (
+	github.com/go-chi/chi/v5 v5.0.8
+	github.com/kelseyhightower/envconfig v1.4.0
+	github.com/rs/zerolog v1.29.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/mocha-bot/enjot/config"
+
+	httpInternal "github.com/mocha-bot/enjot/handler/http"
+	"github.com/mocha-bot/enjot/pkg/logger"
+)
+
+func main() {
+	cfg := config.Get()
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+
+	r := chi.NewRouter()
+
+	httpInternal.NewDummyHandler(r)
+
+	srv := &http.Server{
+		Addr:         cfg.Server.Address(),
+		WriteTimeout: cfg.Server.WriteTimeout,
+		ReadTimeout:  cfg.Server.ReadTimeout,
+		IdleTimeout:  cfg.Server.IdleTimeout,
+		Handler:      r,
+	}
+
+	go func() {
+		logger.Info().
+			Msgf("server running at %s", srv.Addr)
+
+		err := srv.ListenAndServe()
+
+		if err != nil && err != http.ErrServerClosed {
+			logger.Error().
+				Err(err).
+				Str("context", "listen.and.serve").
+				Send()
+		}
+	}()
+
+	<-c
+
+	ctxWithTimeout, cancel := context.WithTimeout(context.Background(), cfg.Server.WaitGracefulTimeout)
+	defer cancel()
+
+	err := srv.Shutdown(ctxWithTimeout)
+
+	if err != nil {
+		logger.Error().
+			Err(err).
+			Str("context", "shutdown").
+			Send()
+	}
+
+	logger.Info().Msg("shutting down")
+}
